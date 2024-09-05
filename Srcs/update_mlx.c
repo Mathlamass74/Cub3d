86dfb1b4901ba4acd52489a2ff00f5c90dc12b83
Mathieu Benjamin Lepesqueur <mlepesqu@c2r1s7.42lausanne.ch>
2024-09-05 13:02:45 +0200

    mouse_undle added

--- Srcs/update_mlx.c
+++ Srcs/update_mlx.c
@@ -1,8 +1,18 @@
 
 #include "../Includes/cub3d.h"
 
+int mouse_hook(int button, int x, int y)
+{
+	if (button == 1)
+		printf("Clic gauche à (%d, %d)\n", x, y);
+	if (button == 2)
+		printf("Clic droit à (%d, %d)\n", x, y);
+	return (0);
+}
+
 void	update_mlx(t_data *d)
 {
 	d->mlx = mlx_init();
 	d->win = mlx_new_window(d->mlx, WIN_WIDTH, WIN_HEIGHT, "Cub3D");
+	mlx_mouse_hook(d->win, mouse_hook, NULL);
 }
