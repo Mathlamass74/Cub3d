25bac97bf8593aa9f2e2e47c457c6b2de2685df2
Mathieu <mathlamass74>
2024-09-19 15:28:17 +0200

    map2

--- Srcs/draw.c
+++ Srcs/draw.c
@@ -1,10 +1,10 @@
 #include "../Includes/cub3d.h"
 
-int draw_map(t_data *d)
+int	draw_map(t_data *d)
 {
-	int x;
-	int y;
-	int s;
+	int	x;
+	int	y;
+	int	s;
 
 	s = 64;
 	y = -1;
@@ -15,13 +15,13 @@ int draw_map(t_data *d)
 		{
 			if (d->map[y][x] == '0')
 				mlx_put_image_to_window(d->mlx, d->win,
-										d->north_texture.text_ptr, x * s, y * s);
+					d->north_texture.text_ptr, x * s, y * s);
 			if (d->map[y][x] == '1')
 				mlx_put_image_to_window(d->mlx, d->win,
-										d->south_texture.text_ptr, x * s, y * s);
+					d->south_texture.text_ptr, x * s, y * s);
 			if (d->map[y][x] == '2')
 				mlx_put_image_to_window(d->mlx, d->win,
-										d->east_texture.text_ptr, x * s, y * s);
+					d->east_texture.text_ptr, x * s, y * s);
 		}
 	}
 	draw_minimap(d);
@@ -72,12 +72,12 @@ void	draw_ray(t_data *d, int p_pos_x, int p_pos_y, t_target *target)
 	}
 }
 
-void draw_player(t_data *d, double pos_x, double pos_y)
+void	draw_player(t_data *d, double pos_x, double pos_y)
 {
-	int start_x;
-	int start_y;
-	int x;
-	int y;
+	int	start_x;
+	int	start_y;
+	int	x;
+	int	y;
 
 	start_x = (int)pos_x - PLAYER_SIZE / 2;
 	start_y = (int)pos_y - PLAYER_SIZE / 2;
